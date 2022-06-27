bbbdc88a3c88c5fb7a1a937c93637ec1339f3b26
Monnavarro <montserrat.nvro.lpz@gmail.com>
2022-06-27 07:00:58 +0200

    simple application of calculator

--- agrparse/calculadora.py
+++ agrparse/calculadora.py
@@ -5,3 +5,18 @@ Created on: 19/6/22
 @author: Montse Navarro <montserrat.nvro.lpz@gmail.com>
 Licence,
 """
+# calculadora.py
+
+# Tipo de operacion: suma/resta/multiplicación
+operacion = "suma"
+
+# Parámetros de la operación
+a = 4
+b = 7
+
+if operacion == "suma":
+    print(a+b)
+elif operacion == "resta":
+    print(a-b)
+elif operacion == "multiplicacion":
+    print(a*b)
